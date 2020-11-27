c70a75eaf9f2a4b80ec5c747d9439f204fba1825
Lizzie Gasson <lizzie.gasson@iag.co.nz>
2020-11-27 15:19:36 +1300

    Display images from search

---
+++ src/components/ImageList.js
@@ -0,0 +1,14 @@
+import React from'react';
+import './dist/ImageList.css';
+
+const ImageList = (props) => {
+    
+    const images = props.images.map(({description, id, urls}) => {
+        return <img className="image" key={id} src={urls.regular} alt={description}/>
+    });
+
+return <div className="image-list">{images}</div>
+    
+};
+
+export default ImageList;
