c8039a05975d52ca74d147c489dbfaf2f94e408c
Lizzie Gasson <lizzie.gasson@iag.co.nz>
2021-02-05 14:44:26 +1300

    Added default value

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class SearchBar extends React.Component{
 
-    state= { term: ''};
+    state= { term: 'Constellation'};
 
     onFormSubmit = event => {
         event.preventDefault();
